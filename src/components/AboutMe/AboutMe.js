6c05ffb21f5b2a44625a69c155d16aabb6c73724
Thiago Moreira <thiago.moreira.ribeiro@gmail.com>
2022-11-23 09:56:32 -0300

    remove bg-green

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -25,16 +25,14 @@ export default function AboutMe({ language }) {
     return (
         <>
             <section id="about" title="about">
-
                 <main
                     id="resume"
-                    className="flex flex-row portrait:flex-col-reverse  h-full bg-green-200"
+                    className="flex flex-row portrait:flex-col-reverse  h-full "
                 >
                     <div className="flex-col w-2/3 portrait:flex-row portrait:w-full portrait:text-center">
                         <Timeline timeline={timeline} />
                     </div>
                     <div className="flex-col w-1/3 portrait:flex-row portrait:w-full portrait:text-center p-8 bg-thi5 text-thi1 overflow-auto">
-
                         <div className=" mb-2 ">
                             <h1 className="text-center text-3xl font-semibold mb-2 text-thi3">
                                 Thiago Moreira Ribeiro
